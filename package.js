f71ddd67ff4b990be15a2be110f62e919bea6c27
Fallen200 <37514938+Fallen200@users.noreply.github.com>
2018-03-22 21:57:39 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "fade",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "crypto-js": "^3.1.9-1",
+    "db": "^1.0.6",
+    "discord.js": "^11.3.0",
+    "quick": "0.0.6",
+    "quick.db": "^5.3.8"
+  }
+}
